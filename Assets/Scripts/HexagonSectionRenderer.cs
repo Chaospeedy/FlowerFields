c033239f6193af8f313643add77698d25b1a74f2
Chaospeedy <seansdilday@gmail.com>
2024-10-25 22:08:22 -0500

    Updated Hexagon prefab
    Hexagon prefab is now made of multiple sections that support their own material

---
+++ Assets/Scripts/HexagonSectionRenderer.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+
+public class HexagonSectionRenderer : MonoBehaviour
+{
+    private Mesh m_mesh;
+    private MeshFilter m_meshFilter;
+    private MeshRenderer m_meshRenderer;
+
+    private List<Face> m_faces;
+
+    public Material material;
+    public float innerSize;
+    public float outerSize;
+    public float height;
+    public bool isFlatTopped;
+    public int sectionNumber;
+
+    public void Awake(){
+        m_meshFilter = GetComponent<MeshFilter>();
+        m_meshRenderer = GetComponent<MeshRenderer>();
+
+        m_mesh = new Mesh();
+
+        m_mesh.name = "Hex";
+
+        m_meshFilter.mesh = m_mesh;
+        m_meshRenderer.material = material;
+    }
+
+    private void OnEnable(){
+        DrawMesh();
+    }
+
+
+    public void DrawMesh(){
+        DrawFaces(sectionNumber);
+        CombineFaces();
+    }
+
+    private void DrawFaces(int sectionNumber){
+        m_faces = new List<Face>();
+
+        //top faces
+        
+        m_faces.Add(CreateFace(innerSize, outerSize, height / 2f, height / 2f, sectionNumber));
+        
+
+        //bottom faces
+        
+        m_faces.Add(CreateFace(innerSize, outerSize, -height / 2f, -height / 2f, sectionNumber, true));
+        
+
+        //outer faces
+        
+        m_faces.Add(CreateFace(outerSize, outerSize, height / 2f, -height / 2f, sectionNumber, true));
+        
+
+        //inner faces
+        
+        m_faces.Add(CreateFace(innerSize, innerSize, height / 2f, -height / 2f, sectionNumber, false));
+        
+
+    }
+
+    private void CombineFaces(){
+
+        List<Vector3> verticies = new List<Vector3>();
+        List<int> tris = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for(int i = 0; i < m_faces.Count; i++){
+            verticies.AddRange(m_faces[i].vertices);
+            uvs.AddRange(m_faces[i].uvs);
+
+            int offset = (4 * i);
+            foreach(int triangle in m_faces[i].triangles){
+                tris.Add(triangle + offset);
+            }
+        }
+
+        m_mesh.vertices = verticies.ToArray();
+        m_mesh.triangles = tris.ToArray();
+        m_mesh.uv = uvs.ToArray();
+        m_mesh.RecalculateNormals();
+    }
+
+    private Face CreateFace(float innerRad, float outerRad, float heightA, float heightB, int point, bool reverse = false){
+
+        Vector3 pointA = GetPoint(innerRad ,heightB, point);
+        Vector3 pointB = GetPoint(innerRad ,heightB, (point < 5) ? point + 1 : 0);
+        Vector3 pointC = GetPoint(outerRad ,heightA, (point < 5) ? point + 1 : 0);
+        Vector3 pointD = GetPoint(outerRad ,heightA, point);
+
+        List<Vector3> vertices = new List<Vector3>() {pointA, pointB, pointC, pointD};
+        List<int> triangles = new List<int>() {0, 1, 2, 2, 3, 0};
+        List<Vector2> uvs = new List<Vector2>() { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1)};
+
+        if (reverse){
+            vertices.Reverse();
+        }
+
+        return new Face(vertices, triangles, uvs);
+    }
+
+    protected Vector3 GetPoint(float size, float height, int index){
+        float angle_deg = isFlatTopped ? 60 * index : 60*index-30;
+        float angle_rad = Mathf.PI / 180f * angle_deg;
+
+        return new Vector3((size * Mathf.Cos(angle_rad)), height, size * Mathf.Sin(angle_rad));
+    }
+
+}
